dbd348841976362025d81fd4a5765a2988b348c8
DESKTOP-RT48FV9\syk98 <syk9848@gmail.com>
2024-12-10 23:12:09 +0900

    fix: map_web screen

--- lib/screens/map_web_screen.dart
+++ lib/screens/map_web_screen.dart
@@ -1,4 +1,69 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class MapWebScreen extends StatelessWidget {
+  final VoidCallback onNavigateBack;
+
+  // 단일 생성자로 통합
+  const MapWebScreen({
+    Key? key,
+    required this.onNavigateBack,
+  }) : super(key: key);
+
+  static final LatLng myLatLng = LatLng(37.5233273, 126.921252); // 지도 초기화 위치
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text(
+          "우리 동네 지도",
+        ),
+        backgroundColor: Colors.white,
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            flex: 2,
+            child: GoogleMap(
+              initialCameraPosition: CameraPosition(
+                target: myLatLng,
+                zoom: 16,
+              ),
+            ),
+          ),
+          Expanded(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(
+                  Icons.timelapse_outlined,
+                  color: Colors.green,
+                  size: 50.0,
+                ),
+                const SizedBox(height: 20.0),
+                ElevatedButton(
+                  onPressed: () {},
+                  child: Text('편의점!'),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+
+
+
+
+
+/*
+//google map
+import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
 class MapWebScreen extends StatelessWidget {
@@ -24,3 +89,5 @@ class MapWebScreen extends StatelessWidget {
     );
   }
 }
+
+ */
