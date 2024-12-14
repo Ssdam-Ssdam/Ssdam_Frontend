e3be0aa622a85cc9b20d72cbb4ab041f0984086d
Jeong jinyoung <jjyoung2419@gmail.com>
2024-12-15 02:20:20 +0900

    fix: add _filterInquiries & update UI design in inquiryScreen

--- lib/screens/inquiry_screen.dart
+++ lib/screens/inquiry_screen.dart
@@ -14,7 +14,6 @@ class InquiryScreen extends StatefulWidget {
     required this.onNavigateBack,
     required this.onNavigateToInquiryCreate, // 콜백 초기화
     required this.onNavigateToInquiryDetail,
-
   });
 
   @override
@@ -24,6 +23,7 @@ class InquiryScreen extends StatefulWidget {
 class _InquiryScreenState extends State<InquiryScreen> {
   String selectedValue = '전체'; // 드롭다운 초기값
   List<Map<String, dynamic>> inquiries = []; // 문의 데이터 리스트
+  List<Map<String, dynamic>> filteredInquiries = []; // 필터링된 문의 데이터 리스트
   final String url = "http://3.36.62.234:3000/inquiry/view-all"; // 서버 URL
 
   @override
@@ -54,6 +54,7 @@ class _InquiryScreenState extends State<InquiryScreen> {
         if (responseData.containsKey('inquiry')) {
           setState(() {
             inquiries = List<Map<String, dynamic>>.from(responseData['inquiry']);
+            filteredInquiries = List.from(inquiries); // 초기에는 전체 데이터를 필터링된 데이터로 설정
           });
         } else {
           print("문의 데이터가 없습니다: ${response.body}");
@@ -64,6 +65,19 @@ class _InquiryScreenState extends State<InquiryScreen> {
     } catch (error) {
       print("네트워크 오류: $error");
     }
+  }
+
+  void _filterInquiries() {
+    setState(() {
+      if (selectedValue == '전체') {
+        filteredInquiries = List.from(inquiries); // 전체 선택 시 모든 문의 내역을 보여줌
+      } else {
+        filteredInquiries = inquiries.where((inquiry) {
+          String statusText = _getStatusText(inquiry['res_status']);
+          return statusText == selectedValue; // '진행중' 또는 '답변완료'에 맞게 필터링
+        }).toList();
+      }
+    });
   }
 
   String _formatDate(String? dateStr) {
@@ -141,56 +155,49 @@ class _InquiryScreenState extends State<InquiryScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white, // 배경색 흰색 설정
-      body: Padding(
-        // padding: const EdgeInsets.all(16.0),
-        padding: const EdgeInsets.only(right: 16, bottom: 16, left:16, top:23),
-        child: Column(
-          children: [
-            Row(
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(right: 16, bottom: 16, left: 16, top: 30), // 상단 여백 증가
+            child: Row(
               children: [
                 GestureDetector(
                   onTap: widget.onNavigateBack,
-                  child: Image.asset(
-                    'assets/backbutton.png',
-                    height: 40,
+                  child: Icon(
+                    Icons.arrow_back, // 기본 안드로이드 뒤로가기 화살표 아이콘
+                    color: Color(0xFF5F5F5F), // 색상 #5f5f5f 설정
+                    size: 30, // 아이콘 크기 조정
                   ),
                 ),
-                Spacer(),
-                Text(
-                  '1:1 문의',
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontWeight: FontWeight.w600,
-                    fontSize: 25,
-                  ),
-                ),
-                Spacer(),
-                ElevatedButton(
-                  onPressed: widget.onNavigateToInquiryCreate, // InquiryCreateScreen으로 이동
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.white,
-                    side: BorderSide(color: Color(0xFFD9D9D9)),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(20),
-                    ),
-                    minimumSize: Size(90, 42),
-                  ),
+                Spacer(),  // 왼쪽 아이콘과 텍스트 사이에 공간을 추가
+                Padding(
+                  padding: const EdgeInsets.only(right: 32.0), // 왼쪽으로 10px 이동
                   child: Text(
-                    '문의하기',
-                    style: TextStyle(color: Colors.black),
+                    '내 문의 내역',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.w600,
+                      fontSize: 25,
+                    ),
                   ),
                 ),
+                Spacer(),  // 텍스트 오른쪽에도 공간을 추가하여 화면 중앙에 정렬
               ],
             ),
-            SizedBox(height: 20),
-            Row(
-              children: [
-                Expanded(
+          ),
+
+
+          SizedBox(height: 15), // 상단과 문의하기 버튼 사이 간격 증가
+          Row(
+            children: [
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 16.0), // 좌우 여백 추가
                   child: Container(
                     padding: EdgeInsets.symmetric(horizontal: 16.0),
                     decoration: BoxDecoration(
                       border: Border.all(color: Colors.black54),
-                      borderRadius: BorderRadius.circular(8.0),
+                      borderRadius: BorderRadius.circular(20.0), // 드롭다운 박스 둥글게 수정
                     ),
                     child: DropdownButton<String>(
                       value: selectedValue,
@@ -199,15 +206,16 @@ class _InquiryScreenState extends State<InquiryScreen> {
                       onChanged: (String? newValue) {
                         setState(() {
                           selectedValue = newValue!;
+                          _filterInquiries(); // 드롭다운 값이 변경될 때마다 필터링
                         });
                       },
-                      items: <String>['전체']
+                      items: <String>['전체', '진행중', '답변완료']
                           .map<DropdownMenuItem<String>>((String value) {
                         return DropdownMenuItem<String>(
                           value: value,
                           child: Text(
                             value,
-                            style: TextStyle(fontSize: 17),
+                            style: TextStyle(fontSize: 16),
                           ),
                         );
                       }).toList(),
@@ -215,34 +223,36 @@ class _InquiryScreenState extends State<InquiryScreen> {
                     ),
                   ),
                 ),
-              ],
-            ),
-            SizedBox(height: 16),
-            Expanded(
-              child: inquiries.isEmpty
-                  ? Center(
-                child: Text(
-                  '문의 내역이 없습니다.',
-                  style: TextStyle(fontSize: 16, color: Colors.grey),
-                ),
-              )
-                  : ListView.builder(
-                itemCount: inquiries.length,
-                itemBuilder: (context, index) {
-                  final inquiry = inquiries[index];
-                  return GestureDetector(
-                    onTap: () async {
-                      final inquiryId = inquiry['inquiryId'];
-                      final responseData = await _fetchInquiryDetail(inquiryId); // 상세 데이터 가져오기
+              ),
+            ],
+          ),
 
-                      if (responseData != null) {
-                        widget.onNavigateToInquiryDetail(responseData); // 상세 페이지로 이동
-                      } else {
-                        print("상세 데이터를 가져오는 데 실패했습니다.");
-                      }
-                    },
+          SizedBox(height: 16),
+          Expanded(
+            child: filteredInquiries.isEmpty
+                ? Center(
+              child: Text(
+                '문의 내역이 없습니다.',
+                style: TextStyle(fontSize: 16, color: Colors.grey),
+              ),
+            )
+                : ListView.builder(
+              itemCount: filteredInquiries.length,
+              itemBuilder: (context, index) {
+                final inquiry = filteredInquiries[index];
+                return GestureDetector(
+                  onTap: () async {
+                    final inquiryId = inquiry['inquiryId'];
+                    final responseData = await _fetchInquiryDetail(inquiryId); // 상세 데이터 가져오기
 
-
+                    if (responseData != null) {
+                      widget.onNavigateToInquiryDetail(responseData); // 상세 페이지로 이동
+                    } else {
+                      print("상세 데이터를 가져오는 데 실패했습니다.");
+                    }
+                  },
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 16.0), // 좌우 여백 추가
                     child: Column(
                       children: [
                         Card(
@@ -264,29 +274,30 @@ class _InquiryScreenState extends State<InquiryScreen> {
                                       ),
                                     ),
                                     Container(
-                                      padding: EdgeInsets.symmetric(
-                                          vertical: 4.0, horizontal: 12.0),
+                                      padding: EdgeInsets.symmetric(horizontal: 9, vertical: 6),
                                       decoration: BoxDecoration(
-                                        color: Colors.white, // 배경 흰색
                                         border: Border.all(
-                                            color: _getStatusColor(inquiry['res_status'])), // 상태 색상
-                                        borderRadius: BorderRadius.circular(20.0), // 둥근 테두리
+                                          color: _getStatusColor(inquiry['res_status']),
+                                        ),
+                                        borderRadius: BorderRadius.circular(12),
                                       ),
                                       child: Text(
                                         _getStatusText(inquiry['res_status']),
                                         style: TextStyle(
-                                          fontSize: 15,
-                                          color: _getStatusColor(inquiry['res_status']), // 상태 색상
+                                          fontSize: 11,
+                                          fontWeight: FontWeight.w600,
+                                          color: _getStatusColor(inquiry['res_status']),
                                         ),
                                       ),
                                     ),
                                   ],
                                 ),
-                                SizedBox(height: 7),
+                                SizedBox(height: 5),
                                 Text(
-                                  inquiry['title'] ?? '',
+                                  inquiry['title'],
                                   style: TextStyle(
-                                    fontSize: 18,
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.w600,
                                     color: Colors.black,
                                   ),
                                 ),
@@ -297,12 +308,36 @@ class _InquiryScreenState extends State<InquiryScreen> {
                         Divider(),
                       ],
                     ),
-                  );
-                },
+                  ),
+                );
+              },
+            ),
+          ),
+          SizedBox(height: 16), // 버튼과 리스트 간격을 더 여유 있게 설정
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16.0), // 좌우 여백만 남김
+            child: GestureDetector(
+              onTap: widget.onNavigateToInquiryCreate, // 문의하기 화면으로 이동
+              child: Container(
+                width: double.infinity,
+                padding: EdgeInsets.symmetric(vertical: 14.0), // 버튼의 세로 길이에 맞게 여백 설정
+                decoration: BoxDecoration(
+                  color: Color(0xFF6EB681), // 배경색 설정
+                  borderRadius: BorderRadius.circular(12.0),
+                ),
+                child: Text(
+                  '문의하기',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 18,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
               ),
-            )
-          ],
-        ),
+            ),
+          )
+        ],
       ),
     );
   }
